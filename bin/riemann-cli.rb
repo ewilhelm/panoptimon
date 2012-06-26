9133e04a446692b8f1f9f7daaf2e29d5f2bc2bae
Benjamin Krueger <ben.krueger@gmail.com>
2012-06-26 16:15:17 -0700

    Initial commit of riemann-cli tool. Addresses SNI/riemann-agent issue #2

---
+++ bin/riemann-cli.rb
@@ -0,0 +1,33 @@
+#!/opt/ruby/bin/ruby
+#
+# Riemann data feeder
+# Feed json directly to a Riemann server
+#
+# Copyright Sourcefire, 2012
+# Benjamin Krueger <bkrueger@sourcefire.com>
+
+require 'rubygems'
+require 'json'
+require 'riemann/client'
+
+input_json = JSON[STDIN.read]
+
+puts "Service: #{input_json['service']}"
+puts "State: #{input_json['state']}"
+puts "Metric: #{input_json['metric']}"
+puts "Description: #{input_json['description']}"
+
+def submitEvent(event)
+  # New Riemann client
+  c = Riemann::Client.new host: 'telemetry.dev1.immunet.com', port: 5555
+
+  # Send event to Riemann server
+  c << {
+    service: event['service'],
+    state: event['state'],
+    metric: event['metric'].to_i,
+    description: event['description']
+  }
+end
+
+submitEvent(input_json)
