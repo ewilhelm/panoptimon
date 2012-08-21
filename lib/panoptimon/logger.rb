dd46546c8128158680926426db9399f054bb838f
Eric Wilhelm <ewilhelm@sourcefire.com>
2012-08-21 10:46:01 -0700

    lib/panoptimon/logger.rb - correctly shared logger

--- lib/panoptimon/logger.rb
+++ lib/panoptimon/logger.rb
@@ -3,6 +3,10 @@ require "logger"
 module Panoptimon::Logger
 
   def logger
+    Panoptimon::Logger.logger
+  end
+
+  def self.logger
     @logger ||= Logger.new($stderr).tap {|l|
       # TODO env setting log level?
       l.level = Logger::WARN}
