9a4efc8449dbbe90c143eaedeb542adacdbde917
Eric Wilhelm <ewilhelm@sourcefire.com>
2012-09-27 13:27:28 -0700

    spec/passthru_spec.rb - popen not %x{}

--- spec/passthru_spec.rb
+++ spec/passthru_spec.rb
@@ -5,7 +5,9 @@ require 'panoptimon'
 describe('pass data through the bus') {
   it('passes') {
     (env = ENV.clone)['RUBYOPT']='-Ilib'; ENV.stub('[]') {|x| env[x]}
-    %x{#{Gem.ruby} ./bin/panoptimon -C sample_configs/passthru/ -D}.
-      should == "ok\n"
+    IO.popen([Gem.ruby, './bin/panoptimon', '-D',
+        '-C', 'sample_configs/passthru/'],
+      'r', {'RUBYOPT' => '-Ilib'}
+    ).readlines.should == ["ok\n"]
   }
 }
