4f9da4ff2a6deeb95effeca7e726cc3a93b4b930
Eric Wilhelm <ewilhelm@sourcefire.com>
2012-11-06 22:15:15 -0800

    lib/panoptimon/util.rb - lookup or dispatch, overridable _os() for testing
    spec/util.rb           - tested

--- lib/panoptimon/util.rb
+++ lib/panoptimon/util.rb
@@ -2,13 +2,19 @@ module Panoptimon
   module Util
     VERSION = '0.0.1'
 
+    def _os; @os ||= Gem::Platform.local.os.to_sym; end
+
+    # return osname
+    # or, given a hash, return the corresponding hash element and raise
+    # error if os not in hash keys
     def self.os (dispatch={})
       # TODO or mess with rbconfig + Config::CONFIG['host_os']
-      @os ||= Gem::Platform.local.os
-      return @os unless dispatch.length > 0
+      os = _os
+      return os unless dispatch.length > 0
 
-      it = dispatch[@os.to_sym] or raise "unsupported OS: #{@os}"
-      return it.call()
+      it = dispatch[os] || dispatch[:default] or
+        raise "unsupported OS: #{os}"
+      return it.is_a?(Proc) ? it.call() : it
     end
 
   end
