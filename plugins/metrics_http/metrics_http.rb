9bbd23242d95e446accc7dd9f31c5a824efac12b
Eric Wilhelm <ewilhelm@sourcefire.com>
2012-12-11 01:01:07 -0800

    plugins/metrics_http/metrics_http.rb - basic implementation of this
    lib/panoptimon/http.rb               - correct mount() implementation
    plugins/status_http/status_http.rb   - match more specifically

---
+++ plugins/metrics_http/metrics_http.rb
@@ -0,0 +1,16 @@
+require "json"
+monitor.enable_cache
+monitor.http.mount('/metrics.json', ->(env) {
+  metrics = monitor.cached
+  match = env['PATH_INFO']
+  match = match.length == 0 ? nil : %r{^(?:#{
+    match.sub(/^\//, '').split(/&/).map {|it|
+      Regexp.escape(Rack::Utils.unescape(it))
+    }.join('|')
+  })(?:\||$)}
+  json = JSON.generate(
+    match ? metrics.select {|k,v| k =~ match} : metrics)
+  [200, {'Content-Type' => 'text/json'}, [json]]
+})
+
+return nil
