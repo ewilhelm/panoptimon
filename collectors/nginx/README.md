f5ba792fed8950328f1eebb63d62e61bc56c18b2
Eric Wilhelm <ewilhelm@sourcefire.com>
2012-12-20 01:58:16 -0600

    collectors/nginx - error handling, docs update,
    collectors/nginx/README.md                               - docs, corrected example url to match default
    collectors/nginx/lib/panoptimon-collector-nginx/nginx.rb - report "not a status page" case, deal with defaults (use symbols consistently)
    collectors/nginx/nginx                                   - symbolize_names from json, $LOAD_PATH thing
    collectors/nginx/nginx.json                              - default config

--- collectors/nginx/README.md
+++ collectors/nginx/README.md
@@ -1,30 +1,29 @@
-Description
-===========
+#Description
 
-The Panoptimon collector nginx is intended to retrieve statistics from a running nginx instance. In order to leverage the collector you will need to have HTTPStubStatus module enabled. For further information on how to enable this module see below.
+The Panoptimon collector 'nginx' is intended to retrieve statistics from a
+running nginx instance. The nginx module 'HTTPStubStatus' is required (see
+below.)
 
-Usage
-=====
+# Config
 
-The collector accepts JSON as input and expects to be provided with a URL. If a URL is not provided the collector will default to localhost.
-
-```
-nginx '{"url": "http://localhost/nginx-status"
+```json
+ {"url": "http://localhost/nginx-status"}
 ```
 
-Output will be returned as JSON.
+# Output
 
 ```
 {"requests":"137","writing":"1","total":"1","reading":"0"}
 ```
 
 
-Module
-=======
-In order to for nginx to provide the information the collector requires we must first enable HTTPStubStatus. Within a server definition simply add the following.
+# Requisite Server Setup
+
+The nginx server must provide the information via its HTTPStubStatus
+module. Within a server definition simply add the following.
 
 ```
-location /nginx_status {
+location /nginx-status {
 	stub_status on;
 	access_log   off;
 	allow 127.0.0.1;
--- collectors/nginx/lib/panoptimon-collector-nginx/nginx.rb
+++ collectors/nginx/lib/panoptimon-collector-nginx/nginx.rb
@@ -2,65 +2,44 @@ module Panoptimon
   module Collector
     class Nginx
 
+      attr_reader :uri
       def initialize(options={})
-        @options = default_options.merge!(options)
-      end
-
-      def options
-        @options
+        options = default_options.merge!(options)
+        @uri = URI(options[:url])
       end
 
       def default_options
-        {:url => 'http://localhost/nginx-status'}
-      end
-
-      def uri
-        @uri ||= URI(options['url'])
-      end
-
-      def host
-        uri.host
-      end
-
-      def port
-        uri.port
-      end
-
-      def status_uri
-        uri.request_uri
+        {url: 'http://localhost/nginx-status'}
       end
 
       def connect
-        @connect ||= Net::HTTP.new(host, port)
+        @connect ||= Net::HTTP.new(uri.host, uri.port)
       end
 
       def request
-        @request ||= Net::HTTP::Get.new(status_uri)
+        @request ||= Net::HTTP::Get.new(uri.request_uri)
       end
 
       def response
         @response ||= connect.request(request)
       end
 
-      def info
-        {:total => total, :reading => reading, :writing  => writing, :requests => requests}
+      def info(body=nil)
+        body ||= response.body
+        raise "not a status report page" if body.match('<') and
+          not body.match(/^Active/)
+        begin
+          {
+            requests: body.match('\s+(\d+)\s+(\d+)\s+(\d+)')[3].to_i,
+            total:    body.match('connections:\s(\d+)')[1].to_i,
+            reading:  body.match('Reading:\s(\d+)')[1].to_i,
+            writing:  body.match('Writing:\s(\d+)')[1].to_i
+          }
+        rescue NoMethodError
+          raise "probably not status page"
+        end
       end
 
-      def requests
-        response.body.match('\s+(\d+)\s+(\d+)\s+(\d+)')[3]
-      end
-
-      def total
-        response.body.match('connections:\s(\d+)')[1]
-      end
-
-      def reading
-        response.body.match('Reading:\s(\d+)')[1]
-      end
-
-      def writing
-        response.body.match('Writing:\s(\d+)')[1]
-      end
     end
   end
 end
