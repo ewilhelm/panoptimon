be9c2ed01eaf3bcea563c945ef2a39d7c4f96ca4
Eric Wilhelm <ewilhelm@sourcefire.com>
2013-01-07 02:37:34 -0800

    collectors/haproxy/ - http + info implementation
    collectors/haproxy/lib/panoptimon-collector-haproxy/haproxy.rb - strip html and parse that
    collectors/haproxy/notes.txt                                   - tests scribbles

--- collectors/haproxy/lib/panoptimon-collector-haproxy/haproxy.rb
+++ collectors/haproxy/lib/panoptimon-collector-haproxy/haproxy.rb
@@ -9,6 +9,7 @@ module Panoptimon
       def initialize(options={})
         url = options[:stats_url] || '/var/run/haproxy.sock'
         @stats_url = url.sub(%r{^socket:/}, '')
+        # slash after bare hostname:port
         @stats_url += '/' if @stats_url =~ %r{^https?://[^/]+$}
         @collector = @stats_url !~ %r{^\w+://} \
           ? :stats_from_sock
@@ -48,6 +49,50 @@ module Panoptimon
       end
 
       def self.stats_from_http(uri)
+        # NOTE uri is expected to have trailing slash if needed
+        {
+          stats: _parse_stats_csv(
+            _http_get(uri + ';csv').split(/\n/) ),
+          info: _parse_html_info( _http_get(uri) )
+        }
+      end
+
+      def self._parse_html_info(body)
+        body =~ %r{General\sprocess\sinformation</[^>]+>
+          (.*?)Running\stasks:\s(\d+)/(\d+)<}xm or
+          raise "body: #{body} does not match expectations"
+        p = $1
+        info = {
+          run_queue: $2, # tasks: $3 ?
+        }
+        # TODO proper dishtml?
+        p.gsub!(%r{\s+}, ' ')
+        p.gsub!(%r{<br>}, "\n")
+        p.gsub!(%r{<[^>]+>}, '')
+        p.gsub!(%r{ +}, ' ')
+        { # harvest some numbers
+          pid:           %r{pid =\s+(\d+)},
+          process_num:   %r{process #(\d+)},
+          nbproc:        %r{nbproc = (\d+)},
+          uptime:        %r{uptime = (\d+d \d+h\d+m\d+s)},
+          memmax_mb:     %r{memmax = (unlimited|\d+)},
+          :'ulimit-n' => %r{ulimit-n = (\d+)},
+          maxsock:       %r{maxsock = (\d+)},
+          maxconn:       %r{maxconn = (\d+)},
+          maxpipes:      %r{maxpipes = (\d+)},
+          currcons:      %r{current conns = (\d+)},
+          pipesused:     %r{current pipes = (\d+)/\d+},
+          pipesfree:     %r{current pipes = \d+/(\d+)},
+        }.each {|k,v|
+          got = p.match(v) or raise "no match for #{k} (#{v})"
+          info[k] = got[1].as_number || got[1]
+        }
+
+        vi = body.match(%r{<body>.*?>([^<]+)\ version\ (\d+\.\d+\.\d+),
+          \ released\ (\d{4}/\d{2}/\d{2})}x) or
+          raise "failed to find version info"
+        info.merge!( name: vi[1], version: vi[2], release_date: vi[3] )
+        return info
       end
 
       def self._http_get(uri)
