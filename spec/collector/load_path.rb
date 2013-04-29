53531783af37fb484ec904b08a55c768306e4716
David Crosby <dcrosby@sourcefire.com>
2013-04-29 11:01:45 -0600

    test _load_collector_config

--- spec/collector/load_path.rb
+++ spec/collector/load_path.rb
@@ -10,16 +10,40 @@ describe "PATH search" do
     FileUtils.mkdir_p @testdir
     fakepath = @testdir + ":/should/not/exist/"
     ENV["PATH"] = fakepath
-    @monitor = Panoptimon::Monitor.new
+
+    test_conf_dir = Pathname.new "/tmp/panoptimon/test_conf/"
+    FileUtils.mkdir_p test_conf_dir
+
+    @test_conf_path = test_conf_dir + "thing.json"
+    f = File.new(@test_conf_path, "w")
+    f.write('{"hey": "yo"}')
+    f.close
+
+    @monitor = Panoptimon::Monitor.new(
+      config: Panoptimon.load_options(['-c', '',
+        '-o', 'collector_interval=9',
+        '-o', 'collector_timeout=12',
+        ])
+      )
   end
 
   it "should return nil if no file is found" do
     expect(@monitor._autodetect_collector_command_path("bad_collector")).to be_nil
+
+    @monitor._load_collector_config(@test_conf_path).should include(:command => nil)
   end
 
   it "should return a path if the command is found" do
     path = File.join(@testdir, "pancollect-thing")
     f = File.new(path, "w")
+    f.close
     expect(@monitor._autodetect_collector_command_path("thing")).to eq(path)
+
+    @monitor._load_collector_config(@test_conf_path).should include(:command => path)
+  end
+
+  after(:each) do
+    FileUtils.rm_rf @testdir
+    FileUtils.rm_rf @test_conf_path
   end
 end
