f87083af4c8fe1c2df867da3381ff29c7e79ba87
Eric Wilhelm <ewilhelm@sourcefire.com>
2014-02-14 21:00:56 -0800

    plugins/graphite - with resend buffer

--- plugins/graphite/graphite.rb
+++ plugins/graphite/graphite.rb
@@ -17,23 +17,28 @@ prefix.gsub!(/<%= *(host|domain) *%>/) { hostname[$1] }
 # kludgy reconnect scheme will drop metrics on failure but avoid blocking
 writer = ->(){
   tries = 0
+  buffer = []
   connect = ->() {
     tries += 1
     warn "tries: #{tries}"
+    buffer = buffer.drop(50) if buffer.length > 100
     return if tries > 3 and not(tries % 10 == 0)
     warn "reconnect"
     socket = TCPSocket.open(host, port)
+    buffer.each {|line| socket.write(line)}
+    buffer.clear
     tries = 0
     return socket
   }
   socket = connect[] # connect early -> fail early
   ->(line) {
     begin
-      socket ||= connect[] or return
+      socket ||= connect[] or return nil.tap { buffer.push(line) }
       socket.write(line)
     rescue => err
-      warn "graphite error #{err}"
+      warn "graphite error: #{err}"
       socket = nil
+      buffer.push(line)
     end
   }
 }[]
