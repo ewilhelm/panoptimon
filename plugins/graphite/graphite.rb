4b2f683bd909757f986005bfc4f93d756def1e33
Eric Wilhelm <ewilhelm@sourcefire.com>
2014-02-14 20:58:39 -0800

    plugins/graphite - lossy reconnect with back-off

--- plugins/graphite/graphite.rb
+++ plugins/graphite/graphite.rb
@@ -14,7 +14,29 @@ prefix = config[:prefix] || '<%= host %>'
 
 prefix.gsub!(/<%= *(host|domain) *%>/) { hostname[$1] }
 
-socket = TCPSocket.open(host, port)
+# kludgy reconnect scheme will drop metrics on failure but avoid blocking
+writer = ->(){
+  tries = 0
+  connect = ->() {
+    tries += 1
+    warn "tries: #{tries}"
+    return if tries > 3 and not(tries % 10 == 0)
+    warn "reconnect"
+    socket = TCPSocket.open(host, port)
+    tries = 0
+    return socket
+  }
+  socket = connect[] # connect early -> fail early
+  ->(line) {
+    begin
+      socket ||= connect[] or return
+      socket.write(line)
+    rescue => err
+      warn "graphite error #{err}"
+      socket = nil
+    end
+  }
+}[]
 
 ->(metrics) {
   t = Time.now.to_i
@@ -31,6 +53,6 @@ socket = TCPSocket.open(host, port)
     metric.gsub!(/\|/, '.')
 
     stat = "#{prefix}.#{metric} #{metrics[k]} #{t}"
-    socket.write("#{stat}\n")
+    writer.call("#{stat}\n")
   }
 }
